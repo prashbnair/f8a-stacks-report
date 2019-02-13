ec5d1249546c96f078f2ebd53a7fa160505b6a1b
Samuzzal Choudhury <samuzzal@redhat.com>
2019-02-13 11:33:52 +0530

    Initial revision

---
+++ Makefile
@@ -0,0 +1,32 @@
+ifeq ($(TARGET),rhel)
+  DOCKERFILE := Dockerfile.rhel
+  REPOSITORY := openshiftio/rhel-f8a-stacks-report-scheduler
+else
+  DOCKERFILE := Dockerfile
+  REPOSITORY := openshiftio/f8a-stacks-report-scheduler
+endif
+
+REGISTRY := quay.io
+DEFAULT_TAG=latest
+
+.PHONY: all docker-build fast-docker-build test get-image-name get-image-repository
+
+all: fast-docker-build
+
+docker-build:
+	docker build --no-cache -t $(REGISTRY)/$(REPOSITORY):$(DEFAULT_TAG) -f $(DOCKERFILE) .
+
+fast-docker-build:
+	docker build -t $(REGISTRY)/$(REPOSITORY):$(DEFAULT_TAG) -f $(DOCKERFILE) .
+
+test:
+	./runtests.sh
+
+get-image-name:
+	@echo $(REGISTRY)/$(REPOSITORY):$(DEFAULT_TAG)
+
+get-image-repository:
+	@echo $(REPOSITORY)
+
+get-push-registry:
+	@echo $(REGISTRY)
---
+++ runtests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# test coverage threshold
+COVERAGE_THRESHOLD=60
+
+gc() {
+  retval=$?
+  deactivate
+  rm -rf venv/
+  exit $retval
+}
+
+trap gc EXIT SIGINT
+
+function prepare_venv() {
+    VIRTUALENV=$(which virtualenv) || :
+    if [ -z "$VIRTUALENV" ]
+    then
+        # python34 which is in CentOS does not have virtualenv binary
+        VIRTUALENV=$(which virtualenv-3)
+    fi
+
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    if [ $? -ne 0 ]
+    then
+        printf "%sPython virtual environment can't be initialized%s" "${RED}" "${NORMAL}"
+        exit 1
+    fi
+}
+PYTHONPATH=$(pwd)/f8a_report/
+export PYTHONPATH
+prepare_venv
+pip3 install -r requirements.txt
+
+pip3 install -r tests/requirements.txt
+
+python3 "$(which pytest)" --cov=f8a_report/ --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv tests
+
+codecov --token=3a540a46-f7e9-4050-b36a-97f81b948bcb
+
+echo "Test suite passed \\o/"
