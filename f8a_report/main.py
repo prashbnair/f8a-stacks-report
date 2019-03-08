6b93e76bb882ee91948bb36964fdcfd7d0211604
animuk <animukhe@redhat.com>
2019-03-08 11:59:26 +0530

    Ingestion Report Added

--- f8a_report/main.py
+++ f8a_report/main.py
@@ -23,9 +23,10 @@ def main():
 
     start_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
     end_date = (today - timedelta(days=1)).strftime('%Y-%m-%d')
-    daily_response = r.get_report(start_date, end_date, 'daily')
+    daily_response, ingestion_results = r.get_report(start_date, end_date, 'daily')
     logger.debug('Daily report data from {s} to {e}'.format(s=start_date, e=end_date))
     logger.debug(json.dumps(daily_response, indent=2))
+    logger.debug(json.dumps(ingestion_results, indent=2))
 
     if time_to_generate_monthly_report(today):
         last_day_of_prev_month = date(today.year, today.month, 1) - timedelta(days=1)
